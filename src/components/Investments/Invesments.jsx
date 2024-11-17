52353cc8e1611dbb69a0e5fc1969412774bfe039
daniel <danielcampelo159@gmail.com>
2024-11-17 16:40:08 -0300

    Adicionando Pagina de investimento nao finalizada

--- src/components/Investments/Invesments.jsx
+++ src/components/Investments/Invesments.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
-import './Dashboard.css';
-import Loading from '../../components/loading/Loading';
-import { IoLogOut } from "react-icons/io5";
+import './Investments.css';
+import Loading from '../loading/Loading';
+import { IoLogOut, IoArrowBackCircle } from "react-icons/io5";
+import InvestmentCards from './InvestmentCards/InvestmentCards';
 
-export default function Dashboard() {
+export default function Investments() {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
 
@@ -13,19 +14,15 @@ export default function Dashboard() {
         setLoading(true)
         setTimeout(() => {
             setLoading(false);
-        }, 1500);
+        }, 500);
     }, []);
 
     const backToLogin = () => {
         navigate('/');
     };
 
-    const goToExpenses = () => {
-        navigate('/expenses');
-    };
-
-    const goToInvest = () => {
-        navigate('/investments');
+    const dashboard = () => {
+        navigate('/dashboard');
     };
 
     return (
@@ -37,19 +34,20 @@ export default function Dashboard() {
                     :
                     <div>
                         <div className='header-content'>
-                            <h1>Controle Financeiro</h1>
+                            <IoArrowBackCircle
+                                className='logout-icon'
+                                onClick={dashboard}
+                            />
+                            <h1>Controle Financeiro de Investimentos</h1>
                             <IoLogOut
                                 className='logout-icon'
                                 onClick={backToLogin}
                             />
                         </div>
-                        <div className='container-value-dash'>
-                            <div className='container-bankcard-dashboard' onClick={goToExpenses}>
-                                <h1>Gastos do Mês</h1>
-                            </div>
-                            <div className='container-bankcard-dashboard' onClick={goToInvest}>
-                                <h1>Investimentos</h1>
-                            </div>
+                        <div className='container-value'>
+                            <InvestmentCards link='sofisa-invest' name='Banco Sofisa Investimentos' />
+                            <InvestmentCards link='nu-invest' name='Banco Nubank Investimentos' />
+                            <p>Função ainda não implementada pelo Dev!</p>
                         </div>
                     </div>
             }
